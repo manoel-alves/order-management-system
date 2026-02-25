46feacd9044726ce53105262270d88faf4015ae1
manoel-alves <manoelann@hotmail.com>
2026-02-25 14:37:28 -0300

    feat(domain): add MoneyConstants for monetary standardization across domain
    - Add MONEY_SCALE to define decimal precision for monetary values
    - add MONEY_ROUNDING to centralize rounding rules

---
+++ backend/src/main/java/br/com/manoel/ordermanagement/model/MoneyConstants.java
@@ -0,0 +1,8 @@
+package br.com.manoel.ordermanagement.model;
+
+import java.math.RoundingMode;
+
+public class MoneyConstants {
+    public static final int MONEY_SCALE = 2;
+    public static final RoundingMode MONEY_ROUNDING = RoundingMode.HALF_UP;
+}
